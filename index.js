8f57c4db9860a4b4225a0d68b3d75c26718459ef
Carl Peaslee <cpeaslee@gmail.com>
2017-07-11 11:32:45 -0500

    first

---
+++ index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <h3>
+        Hi this is baby!
+      </h3>
+    )
+  }
+}
+
+export default App
