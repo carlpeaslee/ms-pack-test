815fb0fae60813ad528ac5161563629bf574d0de
Carl Peaslee <cpeaslee@gmail.com>
2017-07-12 10:01:08 -0500

    does this work now?

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react'
 import { createStore } from 'redux'
+import {connect} from 'react-redux'
 
 
-const store = createStore(reducer)
+export const store = createStore(
+  reducer,
+  loadState()
+)
 store.subscribe(()=>{
   saveState(store.getState())
 })
 
 class App extends Component {
+
   render() {
+    console.log(this)
     return (
       <div>
         <h3>
-          Hi this is baby!
+          Hi this is baby! {this.props.counter}
         </h3>
         <button
           onClick={()=>store.dispatch({type: "INCREMENT"})}
@@ -29,15 +35,30 @@ class App extends Component {
   }
 }
 
+const mapStateToProps = state => {
+  return {
+    counter: state.counter
+  }
+}
+
+
+App = connect(
+  mapStateToProps
+)(App)
+
 export default App
 
 
 function reducer (state = {counter: 0}, action) {
   switch (action.type) {
     case 'INCREMENT':
-      return state.counter + 1
+      return {
+        counter: state.counter + 1
+      }
     case 'DECREMENT':
-      return state.counter - 1
+      return {
+        counter: state.counter - 1
+      }
     default:
       return state
   }
@@ -49,7 +70,7 @@ function loadState() {
     if (stringState === null) {
       return undefined
     }
-    return JSON.parse(serializedState)
+    return JSON.parse(stringState)
   } catch (err) {
     console.log(err)
     return undefined
